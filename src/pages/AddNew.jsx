bf64bbc04f941eec6d025330f568478ca7cc0b40
imas-1 <samispiridon15@gmail.com>
2026-09-15 23:21:58 +0300

    Etapa 2: AddNew navigates to real forms

--- src/pages/AddNew.jsx
+++ src/pages/AddNew.jsx
@@ -7,8 +7,12 @@ export default function AddNew() {
     <div className="vesta-screen">
       <h1 className="display" style={{ fontSize: 28 }}>Adauga</h1>
       <div style={{ display: 'flex', flexDirection: 'column', gap: 12 }}>
-        <button className="btn btn-secondary btn-block" disabled>Adauga o haina (etapa urmatoare)</button>
-        <button className="btn btn-secondary btn-block" disabled>Creeaza o tinuta (etapa urmatoare)</button>
+        <button className="btn btn-secondary btn-block" onClick={() => navigate('/garderoba', { state: { openAdd: true } })}>
+          Adauga o haina
+        </button>
+        <button className="btn btn-secondary btn-block" onClick={() => navigate('/outfits', { state: { openAdd: true } })}>
+          Creeaza o tinuta
+        </button>
         <button className="btn btn-text" onClick={() => navigate(-1)}>Inapoi</button>
       </div>
     </div>
